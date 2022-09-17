3e14077d0dca3373be22c7c152efe08ec7b71cab
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-09-17 07:20:28 +0530

    Add files via upload
    Insertion Sort Algorithm.

---
+++ Insertion Sorting.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void insert(int a[], int n) /* function to sort an aay with insertion sort */
+{
+    int i, j, temp;
+    for (i = 1; i < n; i++) {
+        temp = a[i];
+        j = i - 1;
+
+        while(j>=0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/
+        {
+            a[j+1] = a[j];
+            j = j-1;
+        }
+        a[j+1] = temp;
+    }
+}
+
+void printArr(int a[], int n) /* function to print the array */
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+}
+
+int main()
+{
+    //int a[] = { 12, 31, 25, 8, 32, 17 };
+    //int n = sizeof(a) / sizeof(a[0]);
+    int n, a[1000];
+    printf("\nEnter the size of unsorted array = ");
+    scanf("%d",&n);
+
+    printf("\nEnter the array elements!");
+    for(int i=0;i<n;i++)
+    {
+        printf("\nElement in a[%d] is = ",i);
+        scanf("%d",&a[i]);
+    }
+
+    printf("Before sorting array elements are - \n");
+    printArr(a, n);
+    insert(a, n);
+    printf("\nAfter sorting array elements are - \n");
+    printArr(a, n);
+
+    return 0;
+}
